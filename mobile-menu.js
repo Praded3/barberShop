0857222279134335eea695de3b2a84a14f00afb1
Praded3 <125351697+Praded3@users.noreply.github.com>
2023-04-07 01:35:58 +0300

    vds

--- mobile-menu.js
+++ mobile-menu.js
@@ -1,16 +1,16 @@
 const menuBody = document.querySelector(".mobile-menu");
-const qqq = document.querySelector(".body");
+const body = document.querySelector(".body");
 
 document.addEventListener("click", menu);
 
 function menu(event) {
   if (event.target.closest(".menu-btn")) {
     menuBody.classList.toggle("is-open");
-    qqq.classList.toggle("lock");
+    body.classList.toggle("lock");
   }
+
   if (event.target.closest(".mobile-menu")) {
-    menuBody.classList.remove("is-open");
-    qqq.classList.remove("lock");
+    menuBody.classList.toggle("is-open");
+    body.classList.toggle("lock");
   }
-  
 }
